c4f999464c700ac3b17fe219b8d57258b40306b7
RafaelFernandez <rafaelfernandez0512@gmail.com>
2021-09-23 00:14:08 -0400

    Kata 06 Anagrams

---
+++ Anagrams.Core/Anagrams.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Anagrams.Core
+{
+    public static class Anagrams
+    {
+        public static Dictionary<string,string> FindAnagrams(string path,string paramaters=null)
+        {
+            ValidateFindAnagrams(path);
+            var file = File.ReadAllLines(path);
+            if (paramaters==null)
+            {
+                return file.ToDictionary(e => e);
+            }
+            var words = paramaters?.Split(",");
+            var anagrams = new Dictionary<string, string>();
+            if (!file.Any(e => words.Contains(e)))
+                return anagrams;
+          
+                foreach (var word in file.GroupBy(e => string.Concat(e.OrderBy(x => x))).Where(e=>words.Contains(e.Key)||words.Any(e=>words.Equals(e))))
+                {
+                    if (word.Count()>1)
+                    {
+                        anagrams.Add(word.First(),string.Join(" - ",word));
+                    }
+                }
+         
+            return  anagrams;
+        }
+
+        private static void ValidateFindAnagrams(string input)
+        {
+            if(input==null)
+                throw new NullReferenceException();
+            if (!File.Exists(input))
+                throw new FileNotFoundException();
+        }
+    }
+}
---
+++ KataAnagrams.Test/FindAnagramsTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace KataAnagrams.Test
+{
+    public class FindAnagramsTest
+    {
+        private readonly string _path = System.IO.Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.Parent.Parent.Parent.FullName,"wordlist.txt") ;
+        [Test]
+        public void FindAnagrams_InputNull_ReturnNullReferenceException()
+        {
+            Assert.Catch<NullReferenceException>( ()=>Anagrams.Core.Anagrams.FindAnagrams(null));
+        }
+
+        [Test]
+        public void FindAnagrams_FileInvalid_ReturnFileFoundException()
+        {
+            var expect = "wordlist";
+            Assert.Catch<FileNotFoundException>( ()=>Anagrams.Core.Anagrams.FindAnagrams(expect));
+        }
+
+        [Test]
+        public void FindAnagrams_FileContentsLines_ReturnsNoDictionaryNotEmpty()
+        {
+            Assert.IsNotEmpty(Anagrams.Core.Anagrams.FindAnagrams(_path));
+        }
+        [Test]
+        public void FindAnagrams_WordsAreContentInDictionary_ReturnDictionaryWithAnagrams()
+        {
+            var expect = new Dictionary<string, string>()
+            {
+                ["AAM"] = "AAM - AMA"
+            };
+            var parameter = "AAM";
+            var result = Anagrams.Core.Anagrams.FindAnagrams(_path, parameter);
+            Assert.AreEqual(expect,result);
+        }
+        [Test]
+        public void FindAnagramas_WordsSeparateByCommaAndContentsDictionary_ReturnDictionaryWithAnagrams()
+        {
+            var expect = new Dictionary<string, string>()
+            {
+                ["AAM"] = "AAM - AMA",
+                ["ACT"] = "ACT - ATC - CAT"
+            };
+            var parameter = "AAM,ACT";
+            var result = Anagrams.Core.Anagrams.FindAnagrams(_path, parameter);
+            Assert.AreEqual(expect,result);
+            
+        }
+    }
+}
